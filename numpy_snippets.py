3835918230deac0914c1cd6898d42cb8b7f78378
eric373 <ericryanco@gmail.com>
2018-01-29 21:52:00 -0500

    Added snippets for array creation, basic operations, and indexing

--- numpy_snippets.py
+++ numpy_snippets.py
@@ -28,3 +28,88 @@ e = np.ones( [2,3] )                # preallocate an array with ones
 f = np.empty( (3,4) )               # preallocate an array, output may vary
 g = np.arange( 1, 20, 2 )           # array from 1 to 20 in steps of 2
 h = np.linspace( 4, 40, 7 )         # array from 4 to 40 with 7 numbers
+i = np.array( [[1,2],[3,4]] )
+j = np.array( [[5,6],[7,8]] )
+k = np.random.random( [3,4] )       # create a 3x4 array with random elements
+k = np.round( k, 4 )                # round the elements of the array
+l = np.array( [[1,2,3],[4,5,6],[7,8,9],[10,11,12]] )
+print( '-------------------------------------------------------' )
+print( 'Array creation:' )
+print( 'a = ' + str( a ) )
+print( 'b = ' + str( b ) )
+print( 'c = ' + str( c ) )
+print( 'd = \n' + str( d ) )
+print( 'e = \n' + str( e ) )
+print( 'f = \n' + str( f ) )
+print( 'g = ' + str( g ) )
+print( 'h = ' + str( h ) )
+print( 'i = \n' + str( i ) )
+print( 'j = \n' + str( j ) )
+print( 'k = \n' + str( k ) )
+print( 'l = \n' + str( l ) )
+print( '' )
+
+#**********************************************************************
+#
+# BASIC ARRAY OPERATIONS
+#
+#**********************************************************************
+print( '-------------------------------------------------------' )
+print( 'Basic array operations:' )
+print( 'a + b   = ' + str( a + b ) )
+print( 'a - b   = ' + str( a - b ) )
+print( 'a * b   = ' + str( a * b ) )
+print( 'b / a   = ' + str( b / a ) )
+print( 'b**2    = ' + str( b**2 ) )
+print( 'b < 4.5 = ' + str( b < 4.5 ) )
+print( 'i matrix product j = \n' + str( np.dot( i,j ) ) )
+a += 3; print( 'a += 3 = \n' + str( a ) )   # in-place addition
+a *= 2; print( 'a *= 2 = \n' + str( a ) )   # in-place multiplication
+print( 'sum( k ) = ' + str( k.sum() ) )
+print( 'min( k ) = ' + str( k.min() ) )
+print( 'max( k ) = ' + str( k.max() ) )
+print( 'cumsum( k ) = \n' + str( k.cumsum() ) )
+print( 'sumOfColumns( k ) = ' + str( k.sum( axis = 1 ) ) )
+print( 'minOfColumns( k ) = ' + str( k.min( axis = 1 ) ) )
+print( 'maxOfColumns( k ) = ' + str( k.max( axis = 1 ) ) )
+print( 'cumsumOfColumns( k ) = \n' + str( k.cumsum( axis = 1 ) ) )
+print( '' )
+
+#**********************************************************************
+#
+# UNIVERSAL FUNCTIONS
+#
+#**********************************************************************
+print( '-------------------------------------------------------' )
+print( 'Some universal functions:' )
+print( 'exp( a )  = ' + str( np.exp(a) ) )
+print( 'sqrt( a ) = ' + str( np.sqrt(a) ) )
+print( 'a + b     = ' + str( np.add(a,b) ) )
+print( 'sin( a )  = ' + str( np.sin(a) ) )
+print( '' )
+
+#**********************************************************************
+#
+# INDEXING, SLICING AND ITERATING
+#
+#**********************************************************************
+x = np.round( k.cumsum() ** 3, 4 )
+print( '-------------------------------------------------------' )
+print( 'Indexing 1D arrays:' ) 
+print( 'All elements of x = \n' + str( x[::] ) )
+print( '5th element of x         = ', str( x[4] ) )
+print( '4th to 6th elements of x = ', str( x[3:6] ) )
+print( '1st element of x         = ', str( x[0] ) )
+print( 'Last element of x        = ', str( x[-1] ) )
+print( 'All but first 2 elements of x = \n', str( x[2:] ) )
+print( 'All but last 2 elements of x = \n', str( x[:-2] ) )
+print( 'Every 2nd element of x = \n', str( x[::2] ) )
+print( 'Every 3rd element of x = \n', str( x[::3] ) )
+print( 'Reversed x = \n', str( x[::-1] ) )
+print( '' )
+print( '-------------------------------------------------------' )
+print( 'Indexing 2D arrays:' )
+print( 'Row 3 column 2 of l = \n' + str( l[2,1] ) )
+print( 'Row 3 of l = \n' + str( l[2,:] ) )
+print( 'Column 3 of l = \n' + str( l[:,2] ) )
+print( 'Rows 2 and 3 of l = \n' + str( l[1:3,:] ) )
