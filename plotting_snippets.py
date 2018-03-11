ab26d23d7fc2750580007925c77c5e3acd70b4a8
eric373 <ericryanco@gmail.com>
2018-03-11 19:12:38 -0400

    Added extra formatting such as legend suppression and secondary y axis

--- plotting_snippets.py
+++ plotting_snippets.py
@@ -1,5 +1,6 @@
 import subprocess as sp
 import matplotlib.pyplot as plt
+import matplotlib as mpl
 import numpy as np
 import pandas as pd
 
@@ -20,7 +21,13 @@ cntPt = 200
 # PANDAS TIME-SERIES PLOTS
 #
 #***********************************************************************
-# TODO: explore plot formatting
+# see avaialbe plotting styles in matplotlib
+print( 'Available plotting styles in matplotlib:' )
+for style in mpl.style.available:
+    print( style )
+
+# change the style of matplotlib plotting
+mpl.style.use( 'seaborn' )
 
 # plot cumulative sum of a time series in a Series type
 ts = pd.Series( np.random.randn( cntPt )    # create a time series
@@ -32,7 +39,7 @@ lsFig.append( ax.get_figure() )             # store figure handle
 
 # plot cumulative sum of multiple time series in a DataFrame type
 df = pd.DataFrame( np.random.randn( cntPt, 3 )
-                 , index = df.index
+                 , index = ts.index
                  , columns = list('ABC') ); # create a DataFrame with 3 columns
 df = df.cumsum()                            # calculate cumulative sums per column
 ax = df.plot()                              # plot cumulative sums
@@ -45,6 +52,16 @@ df['D'] = pd.Series( list( range( len( df ) ) )
 ax = df.plot( x = 'D', y = 'A' )            # plot A against D
 lsAx.append( ax )                           # store axis handle
 lsFig.append( ax.get_figure() )             # store figure handle
+
+# plot x and y with secondary y axis
+ax = df.plot( x = 'D', y = 'A'              # plot column A against column D
+            , legend = False )              # without legend
+ax2 = df.plot( x = 'D', y = ['B','C']       # plot column B and C against column D
+            , ax = ax                       # on the same figure
+            , mark_right = False            # don't mark (right) in legend for columns in secondary axis
+            , secondary_y = True )          # on a secondary y axis
+ax.set_ylabel( 'Main Axis' )
+ax2.set_ylabel( 'Secondary Axis' )
 
 # show all plots
 plt.show()
