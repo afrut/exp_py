bb94e3e39074a60e1e868fd854cd305845d6444b
eric373 <ericryanco@gmail.com>
2018-03-28 21:18:30 -0400

    Added boxplot, x labels, y labels, titles, histogram, and scatter matrix

--- plotting_pandas_snippets.py
+++ plotting_pandas_snippets.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 import matplotlib as mpl
 import numpy as np
 import pandas as pd
+import sklearn.datasets as datasets
 
 # clear screen
 sp.call( 'cls', shell = True )
@@ -22,7 +23,7 @@ for style in mpl.style.available:
     print( style )
 
 # change the style of matplotlib plotting
-mpl.style.use( 'seaborn' )
+#mpl.style.use( 'classic' )
 
 # plot cumulative sum of a time series in a Series type
 ts = pd.Series( np.random.randn( cntPt )    # create a time series
@@ -31,6 +32,9 @@ ts = ts.cumsum()                            # calculate cumulative sum
 ax = ts.plot()                              # plot on the figure
 lsAx.append( ax )                           # store axis handle
 lsFig.append( ax.get_figure() )             # store figure handle
+ax.set_xlabel( 'Day' )                      # x-axis label
+ax.set_ylabel( 'Y value' )                  # y-axis label
+ax.set_title( 'Time Series Plot' )          # plot title
 
 # plot cumulative sum of multiple time series in a DataFrame type
 df = pd.DataFrame( np.random.randn( cntPt, 3 )
@@ -40,6 +44,9 @@ df = df.cumsum()                            # calculate cumulative sums per colu
 ax = df.plot()                              # plot cumulative sums
 lsAx.append( ax )                           # store axis handle
 lsFig.append( ax.get_figure() )             # store figure handle
+ax.set_xlabel( 'Day' )                      # x-axis label
+ax.set_ylabel( 'Y value' )                  # y-axis label
+ax.set_title( 'Multiple Time Series Plot' ) # plot title
 
 # plot x and y specifying columns of a DataFrame type
 df['D'] = pd.Series( list( range( len( df ) ) )
@@ -47,6 +54,9 @@ df['D'] = pd.Series( list( range( len( df ) ) )
 ax = df.plot( x = 'D', y = 'A' )            # plot A against D
 lsAx.append( ax )                           # store axis handle
 lsFig.append( ax.get_figure() )             # store figure handle
+ax.set_xlabel( 'Column D' )                 # x-axis label
+ax.set_ylabel( 'Column A' )                 # y-axis label
+ax.set_title( 'Column A against Column D' ) # plot title
 
 # plot x and y with secondary y axis
 ax = df.plot( x = 'D', y = 'A'              # plot column A against column D
@@ -59,18 +69,48 @@ ax2 = df.plot( x = 'D', y = 'C'             # plot column C against column D
             , ax = ax                       # on the same figure
             , mark_right = False            # don't mark (right) in legend for columns in secondary axis
             , secondary_y = True )          # on a secondary y axis
+ax.set_xlabel( 'X axis' )                   # x-axis label
 ax.set_ylabel( 'Main Axis' )                # Axis title for primary y-axis
 ax2.set_ylabel( 'Secondary Axis' )          # Axis title for secondary y-axis
+ax.set_title( 'Multiple Y Axes' )
 
 # tick resolution adjustment suppression
 with pd.plotting.plot_params.use( 'x_compat', True ):   # do not automatically adjust x-axis resolution
     ax = df.loc[:, list( set( df.columns ).difference( ['D'] ) ) ].plot() # plot all  columns except D
+ax.set_xlabel( 'Day' )
+ax.set_ylabel( 'Y value' )
+ax.set_title( 'Suppress Automatic X-axis Resolution Adjustment' )
+
+#*********************************************************************
+#
+# STATISTICAL PLOTS
+#
+#*********************************************************************
+
+# load the iris dataset
+iris = datasets.load_iris()
+df = pd.DataFrame( iris.data, columns = iris.feature_names )
+
+# box plot
+ax = df.plot( kind = 'box'
+            , subplots = True
+            , layout = (2,2)
+            , sharex = False
+            , sharey = False )
+fig = ax[0].get_figure()
+fig.suptitle( 'Boxplot of Features of Iris Dataset' )   # overall title
+
+# histogram
+ax = np.squeeze( df.hist() )
+fig = ax[0][0].get_figure()
+fig.suptitle( 'Histogram of Features of Iris Dataset' )
+
+# scatter_matrix
+ax = pd.plotting.scatter_matrix( df, alpha = 1 )
+fig = ax[0][0].get_figure()
+fig.suptitle( 'Scatter Matrix of Features of Iris Dataset' )
 
 # TODO: explore subplots
 
 # show all plots
 plt.show()
-
-# TODO: box plot
-# TODO: histogram
-# TODO: scatter_matrix
